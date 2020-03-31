efcfaa6a955eadb6e5e4a7104c868b2c09fa8cbc
Chloe Edeal <caedeal@gmail.com>
2020-03-30 18:37:42 -0600

    Adding gql and rollup config

--- src/components/manifold-subscription-list/manifold-subscription-list.tsx
+++ src/components/manifold-subscription-list/manifold-subscription-list.tsx
@@ -1,29 +1,27 @@
-import { Component, h, Element } from '@stencil/core';
-import { Connection } from '@manifoldco/manifold-init-types/types/v0';
+import { Component, h, Element } from "@stencil/core";
+import { Connection } from "@manifoldco/manifold-init-types/types/v0";
 
 @Component({
-    tag: 'manifold-subscription-list',
-    styleUrl: 'manifold-subscription-list.css',
-  })
-  export class ManifoldSubscriptionList {
-    @Element() el: HTMLElement;
-  
-    connection: Connection;
-  
-    async componentWillLoad() {
-      await customElements.whenDefined('manifold-init');
-      const core = document.querySelector('manifold-init') as HTMLManifoldInitElement;
-      this.connection = await core.initialize({
-        element: this.el,
-        componentVersion: '<@NPM_PACKAGE_VERSION@>',
-        version: 0,
-      });
-    }
-    render() {
-        return (
-          <div class="ManifoldSubscriptionList">
-            Hello out there
-          </div>
-        );
-    }
+  tag: "manifold-subscription-list",
+  styleUrl: "manifold-subscription-list.css"
+})
+export class ManifoldSubscriptionList {
+  @Element() el: HTMLElement;
+
+  connection: Connection;
+
+  async componentWillLoad() {
+    await customElements.whenDefined("manifold-init");
+    const core = document.querySelector(
+      "manifold-init"
+    ) as HTMLManifoldInitElement;
+    this.connection = await core.initialize({
+      element: this.el,
+      componentVersion: "<@NPM_PACKAGE_VERSION@>",
+      version: 0
+    });
+  }
+  render() {
+    return <div class="ManifoldSubscriptionList">Hello out there</div>;
+  }
 }
