4bbebff35336225294c815ecb9c1d462ca9dea53
Sam Slotsky <sam@manifold.co>
2020-04-17 11:04:46 -0500

    Display invoice data on the page

--- src/components.d.ts
+++ src/components.d.ts
@@ -11,7 +11,7 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 export namespace Components {
   interface ManifoldInvoices {
-    'clientId'?: string;
+    'heading'?: string;
   }
 }
 
@@ -30,7 +30,7 @@ declare global {
 
 declare namespace LocalJSX {
   interface ManifoldInvoices {
-    'clientId'?: string;
+    'heading'?: string;
   }
 
   interface IntrinsicElements {
